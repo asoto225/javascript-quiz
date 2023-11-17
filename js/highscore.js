21a3a087a5a872b5cb90b4622d15d86d6ae3e425
asoto225 <abelsoto12@hotmail.com>
2023-11-17 13:38:24 -0700

    highscore page fixed

--- js/highscore.js
+++ js/highscore.js
@@ -1,20 +1,23 @@
 document.addEventListener('DOMContentLoaded', displayHighScores);
 
-
-function displayHighScores(){
+function displayHighScores() {
     const highScoreList = document.getElementById('highScores');
     const storedHighScores = JSON.parse(localStorage.getItem('highScores'));
 
-
-    // highScoreList.innerHTML = '';
-
-    storedHighScores.forEach(entry => {
+    if (highScoreList && storedHighScores) {
+        highScoreList.innerHTML = '';
+        storedHighScores.forEach(entry => {
+            const li = document.createElement('li');
+            li.innerText = `${entry.user} - ${entry.score}`;
+            highScoreList.appendChild(li);
+        });
+    } else {
         const li = document.createElement('li');
-        li.innerText = `${entry.user} - ${entry.score}`;
+        li.innerText = 'No high scores yet!';
         highScoreList.appendChild(li);
-    });
+    }
 }
 
-function redirectToQuiz(){
+function redirectToQuiz() {
     window.location.href = 'index.html';
 }
